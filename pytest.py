45044efa89706e7af1cebc6fe8905ebc52fb8270
Timothy Webb <webb.timothy@gmail.com>
2025-05-28 02:07:37 -0400

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -1,12 +1,4 @@
 import unittest
-"""Module has a function that prints the name of my site"""
-
-
-def print_site_name():
-    """Function printing the name of my site"""
-    site = 'github.com/timothygwebb'
-
-    print(site)
 
 class TestBasicFunctionality(unittest.TestCase):
     def test_sample(self):
